3f7de9d449eea80541f04209e60ee0d543bd9974
Alex Olszewski <Olszewski.Alexander@titans.easternflorida.edu>
2022-02-25 18:08:18 -0500

    part 3

---
+++ commands/info/penguins.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "penguins",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply("Lets Go Penguins!")
+    }
+}
